cdf045e5fdeeb7dbef68486a048e44ffd6e2402c
Cody <croge32@gmail.com>
2014-12-07 14:02:33 -0600

    added more validation for form

--- index.php
+++ index.php
@@ -1,3 +1,129 @@
+<?php
+include ('vendor/autoload.php');
+if(isset( $_POST['submit'] ) ) {
+	$sendgrid = new SendGrid('croge32', 'Cr12345432123454321');
+
+	$email = new SendGrid\Email();
+
+	$name = $phoneNumber = $city = $address = $zipCode = "";
+	$brand = $modelNumber = $year = $mounted = $optionsRadios = $symptom = "";
+	$errors = array();
+
+	if(!isset($_REQUEST['name']) || strlen(trim($_REQUEST['name'])) == 0){
+    	array_push($errors, "no name provided");
+	} else {
+		$name = $_REQUEST['name'];
+	}
+
+	if(!isset($_REQUEST['phoneNumber']) || strlen(trim($_REQUEST['phoneNumber'])) == 0){
+    	array_push($errors, "no phone number provided");
+	} else {
+		if (preg_match("/^(\+0?1\s)?\(?\d{3}\)?[\s.-]\d{3}[\s.-]\d{4}$/", $_REQUEST['phoneNumber'])) {
+		    $phoneNumber = $_REQUEST['phoneNumber'];
+		} else {
+    		array_push($errors, "Phone number is invalid");
+		}
+		
+	}
+
+	if(!isset($_REQUEST['city']) || strlen(trim($_REQUEST['city'])) == 0){
+    	array_push($errors, "no city provided");
+	} else {
+		$city = $_REQUEST['city'];
+	}
+
+	if(!isset($_REQUEST['address']) || strlen(trim($_REQUEST['address'])) == 0){
+    	array_push($errors, "no address provided");
+	} else {
+		$address = $_REQUEST['address'];
+	}
+
+	if(!isset($_REQUEST['zipCode']) || strlen(trim($_REQUEST['zipCode'])) == 0){
+    	array_push($errors, "no zip code provided");
+	} else {
+		if (preg_match("/^\d{5}(?:[-\s]\d{4})?$/", $_REQUEST['zipCode'])) {
+		    $zipCode = $_REQUEST['zipCode'];
+		} else {
+    		array_push($errors, "Zip code is invalid");
+		}
+	}
+
+	if(!isset($_REQUEST['brand']) || strlen(trim($_REQUEST['brand'])) == 0){
+    	array_push($errors, "no brand provided");
+	} else {
+		if (preg_match("/^[A-Za-z]+$/", $_REQUEST['brand'])) {
+		    $brand = $_REQUEST['brand'];
+		} else {
+    		array_push($errors, "Brand name is invalid");
+		}
+	}
+
+	if(!isset($_REQUEST['modelNumber']) || strlen(trim($_REQUEST['modelNumber'])) == 0){
+    	array_push($errors, "no model number provided");
+	} else {
+		$modelNumber = $_REQUEST['modelNumber'];
+	}
+
+	if(!isset($_REQUEST['year']) || strlen(trim($_REQUEST['year'])) == 0){
+    	array_push($errors, "no year provided");
+	} else {
+		if (preg_match("/^[12][0-9]{3}$/", $_REQUEST['year'])) {
+		    $year = $_REQUEST['year'];
+		} else {
+    		array_push($errors, "Year is invalid");
+		}
+	}
+
+	if(!isset($_REQUEST['symptom']) || strlen(trim($_REQUEST['symptom'])) == 0){
+    	array_push($errors, "no symptoms provided");
+	} else {
+		$symptom = $_REQUEST['symptom'];
+	}
+
+	if($_REQUEST['mounted'] == true) {
+		$mounted = "Mounted";
+	} else {
+		$mounted = "Not Mounted";
+	}
+
+	if($_REQUEST['optionsRadios'] == "radioSuddenly") {
+		$optionsRadios = "Went out suddenly";
+	} else {
+		$optionsRadios = "Acted Up";
+	}
+
+	if (count($errors) != 0) {
+		echo "<div class='container shadow'><h4>Errors submitting ticket</h4><br>";
+		foreach ($errors as $error) {
+			echo "<h5>$error</h5>";
+		}
+		echo "</div>";
+	} else {
+		$ticket = "<p>Name: $name</p>
+				<p>Phone Number: $phoneNumber</p>
+				<p>City: $city</p>
+				<p>Address: $address</p>
+				<p>ZIP: $zipCode</p>
+				<p>Brand: $brand</p>
+				<p>Model Number: $modelNumber</p>
+				<p>Year: $year</p>
+				<p>Mounted: $mounted</p>
+				<p>Sudden: $optionsRadios</p>
+			   	<p>Symptoms: $symptom</p>";
+
+		$email->addTo('croge32@gmail.com')->
+		       setFrom('foo@bar.com')->
+		       setSubject('Ticket')->
+		       setHtml($ticket);
+
+		$sendgrid->send($email);
+
+		echo '<div class="container shadow"><h4>Ticket submitted successfully! We will call you to set up an appointment shortly.</h4></div>';
+	}
+	
+}
+?>
+
 <html>
 
 <head>
@@ -14,7 +140,6 @@
 </head>
 
 <body>
-
 	
 	<nav class="navbar navbar-default navbar-fixed-top" id="nav" role="navigation">
 	  <div class="container-fluid">
@@ -139,7 +264,7 @@
     			<h3 style="padding-top: 60px; margin-top: -60px;">Repair</h3><br>
 			</a>
 
-			<form role="form" method="POST" name="repair" action="form.php">
+			<form role="form" method="POST" name="repair" action="index.php">
 
 				<div class="row">
 					<div class="col-md-4 col-md-offset-1">
