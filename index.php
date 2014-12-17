52f193a7b5f7a5ac83ec00e34261fb1e70437f4b
Cody Rogers <croge32@lsu.edu>
2014-12-17 01:51:50 -0600

    Fixed some errors

--- index.php
+++ index.php
@@ -124,7 +124,7 @@ if(isset( $_POST['submit'] ) ) {
 
 <head>
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	
+
 	<title>Affordable In-Home Tv Service</title>
 	<meta name="description" content="Affordable In-Home TV Service: Relax, we come to you! Schedule an appointment today!" /> 
 	<meta name="keywords" content="television,repair,service,affordable,fix,broken,baton rouge,gonzales,central,denham springs" />
@@ -133,7 +133,6 @@ if(isset( $_POST['submit'] ) ) {
 	<link rel="stylesheet" href="libs/bootstrap/css/bootstrap-theme.min.css">
 	<link rel="stylesheet" href="css/main.css">
 
-	<script type="text/javascript" src="libs/jquery-2.1.1.min.js"></script>
 	<script type="text/javascript" src="libs/bootstrap/js/bootstrap.min.js"></script>
 	
 </head>
@@ -172,13 +171,17 @@ if(isset( $_POST['submit'] ) ) {
 
 			<h4>Do you have a broken television and no means to purchase a new set or bring your TV to a repair shop?</h4>
 
-		    <p>At Affordable In-Home TV Service, we do all the work for you! Located out of Baton Rouge, Louisiana, our business has served Baton Rouge and the surrounding area for over 30 years. Simply call and we will send a licensed technician to inspect, diagnose, and repair your television, all within the comfort of your own home. We work on every popular brand and many different varieties of televisions, including projection, HDTV LCD, LED, and many more!</p>
+		    <p>At Affordable In-Home TV Service, we do all the work for you! Located out of Baton Rouge, Louisiana, our business has served Baton Rouge 
+		    and the surrounding area for over 30 years. Simply call and we will send a licensed technician to inspect, diagnose, and repair your television, 
+		    all within the comfort of your own home. We work on every popular brand and many different varieties of televisions, including projection, HDTV, LCD, LED, and many more!</p>
 
 		    <p>For a complete list of supported brands and models, visit our <a href="#brands">brands</a> page.</p>
 
 		    <h4>Why in-home repair?</h4>
 
-			<p>With our technician in your home, you can be sure you are receiving the BEST service at a BARGAIN price. With in-home repair, you don't have to fear additional damage to your television through transit to a repair shop or the negligence of a shop repairman. You will stay informed, as our technician will provide a detailed explanation of your television's issue as he repairs it.</p>
+			<p>With our technician in your home, you can be sure you are receiving the BEST service at a BARGAIN price. With in-home repair, you don't have to fear additional damage 
+			to your television through transit to a repair shop or the negligence of a shop repairman. You will stay informed, as our technician will provide a detailed explanation of your 
+			television's issue as he repairs it.</p>
 
 			<h4>Don't overwork yourself! At Affordable In-Home TV Service, you can relax... we come to you!</h4>
 		</div>
@@ -201,7 +204,8 @@ if(isset( $_POST['submit'] ) ) {
 
 			<h5>In Baton Rouge, we service zip codes:</h5>
 
-			<p>70801, 70802, 70803, 70804, 70806, 70807, 70808, 70809, 70810, 70811, 70812, 70813, 70814, 70815, 70816, 70817, 70818, 70819, 70820, 70821, 70822, 70823, 70825, 70826, 70827, 70831, 70833, 70835, 70836, 70837, 70874, 70879, 70883, 70884, 70893, 70894, 70895, 70896, 70898</p>
+			<p>70801, 70802, 70803, 70804, 70806, 70807, 70808, 70809, 70810, 70811, 70812, 70813, 70814, 70815, 70816, 70817, 70818, 70819, 70820, 70821, 70822, 
+			70823, 70825, 70826, 70827, 70831, 70833, 70835, 70836, 70837, 70874, 70879, 70883, 70884, 70893, 70894, 70895, 70896, 70898</p>
 
 			<h5>In Denham Springs, we service zip codes:</h5>
 
@@ -231,7 +235,8 @@ if(isset( $_POST['submit'] ) ) {
 
 			<h4>We service many different television brands including:</h4>
 
-			<p>AKAI, Denon, Emerson, GE, Harmon, Kardon, Hitachi, JVC, LG, Magnavox, Mitsubishi, Philips, Panasonic, Pioneer, Polaroid, Quasar, RCA, Samsung, Sharp, Sony, Sylvania, Teac, Toshiba, Vizio, Westinghouse, Zenith, and many more.<p><br>
+			<p>AKAI, Denon, Emerson, GE, Harmon, Kardon, Hitachi, JVC, LG, Magnavox, Mitsubishi, Philips, Panasonic, Pioneer, Polaroid, Quasar, RCA, 
+			Samsung, Sharp, Sony, Sylvania, Teac, Toshiba, Vizio, Westinghouse, Zenith, and many more.<p><br>
 
 			<h4>We specialize in Big Screen TV, HDTV, Projection TV, DLP TV, LCD TV, LED TV, and VCR repair.</h4><br>
 
@@ -246,7 +251,9 @@ if(isset( $_POST['submit'] ) ) {
     			<h3 style="padding-top: 60px; margin-top: -60px;">About</h3><br>
 			</a>
 
-			<h4>Affordable In-Home TV Service, LLC has been serving South Louisiana since 1984. Our technician is a proud member of NESDA (National Electronic Service Dealers Association) and SESDA (South Eastern Service Dealers Association). He is factory trained and possesses an LA Radio and TV License as well as an F.C.C. License.</h4><br>
+			<h4>Affordable In-Home TV Service, LLC has been serving South Louisiana since 1984. Our technician is a proud member of NESDA 
+			(National Electronic Service Dealers Association) and SESDA (South Eastern Service Dealers Association). 
+			He is factory trained and possesses an LA Radio and TV License as well as an F.C.C. License.</h4><br>
 
 			<h4>Contacting us is easy</h4>
 
